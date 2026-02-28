0ed4ce5de1fc963d6d66be08a6b80aa29439525e
Damien Laureaux <d.laureaux@timoa.com>
2026-02-28 07:30:17 +0100

    chore(project): fix Semantic Release tag format

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -1,6 +1,7 @@
 /** @type {import('semantic-release').GlobalConfig} */
 module.exports = {
-  branches: ['main', 'master'],
+  branches: ['main'],
+  tagFormat: 'v${version}',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
