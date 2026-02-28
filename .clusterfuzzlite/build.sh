9cbddd7339ae3e76914153d34e878fb41dd3987f
Damien Laureaux <d.laureaux@timoa.com>
2026-02-28 09:29:33 +0100

    chore(deps): pin dependencies

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -eu
 
-npm install
-npm install --save-dev @jazzer.js/core
+npm install --no-save yaml@2.8.2 @jazzer.js/core@2.1.0
 
 compile_javascript_fuzzer workflow-metrics .clusterfuzzlite/fuzz_workflow_yaml.js --sync
